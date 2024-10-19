6d0c9512c1d635b197e13541233e1618da28339b
matteoo.eth <mateogalic112@gmail.com>
2024-10-19 22:47:43 +0200

    validation middleware

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,23 @@
+import { BadRequestError } from "errors/bad-request.error";
+import type { Request, Response, NextFunction } from "express";
+import { AnyZodObject, ZodError } from "zod";
+
+const validationMiddleware =
+  (schema: AnyZodObject) =>
+  async (req: Request, _: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      return next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return next(new BadRequestError(error.issues[0].message));
+      }
+      return next(new BadRequestError("Error in validation process."));
+    }
+  };
+
+export default validationMiddleware;
