2b78424c17b0202b762fdd0e9149cf44b4f3e5af
matteoo.eth <mateogalic112@gmail.com>
2024-10-20 09:14:48 +0200

    remove create user endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,24 +10,7 @@ class UsersController extends Controller {
 
   protected initializeRoutes() {
     this.router.get(this.path, this.getUsers);
-    this.router.post(this.path, this.createUser);
   }
-
-  private createUser = async (
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    const { username, email } = request.body;
-    try {
-      const insertUser =
-        "INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *";
-      const result = await pool.query(insertUser, [username, email]);
-      response.json({ createdUser: result.rows[0] });
-    } catch (err) {
-      next(err);
-    }
-  };
 
   private getUsers = async (
     _: Request,
