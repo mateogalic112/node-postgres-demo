79a0ef7caf52d82071fa5c72507fb411babb0841
matteoo.eth <mateogalic112@gmail.com>
2024-10-20 12:31:05 +0200

    remove elint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.{ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ["**/*.ts"] },
+  { languageOptions: { globals: globals.node } },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": [
