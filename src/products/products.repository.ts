462aeee1ca4069f92fc809221b2dd17fd3083d81
Mateo Galic <mateogalic112@gmail.com>
2025-05-16 07:36:24 +0200

    update products query

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -1,26 +1,14 @@
 import pool from "config/database";
 import { Product } from "./products.model";
 import { PaginatedRequestParams } from "interfaces/api.interface";
-import { QueryResult } from "pg";
 import { CreateProductPayload } from "./products.validation";
 
 export class ProductRepository {
   public async getProducts(params: PaginatedRequestParams) {
-    let queryPromise: Promise<QueryResult<Product>>;
-
-    if (!params.cursor) {
-      queryPromise = pool.query<Product>(
-        "SELECT id, name, description, price FROM products ORDER BY id ASC LIMIT $1",
-        [params.limit]
-      );
-    } else {
-      queryPromise = pool.query<Product>(
-        "SELECT id, name, description, price FROM products WHERE id > $1 ORDER BY id ASC LIMIT $2",
-        [params.cursor, params.limit]
-      );
-    }
-
-    const result = await queryPromise;
+    const result = await pool.query<Product>(
+      "SELECT id, name, description, price FROM products WHERE id > $1 ORDER BY id ASC LIMIT $2",
+      [params.cursor ?? 0, params.limit]
+    );
     return result.rows;
   }
 
