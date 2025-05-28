0a276c73cc29d7d755c37bef9f00dee59e39d99f
Mateo Galic <mateogalic112@gmail.com>
2025-05-28 16:12:31 +0200

    update validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -2,22 +2,30 @@ import { BadRequestError } from "errors/http.error";
 import type { RequestHandler } from "express";
 import { ZodError, z } from "zod";
 
+type RequestSchema = z.ZodSchema<{
+  body?: unknown;
+  query?: unknown;
+  params?: unknown;
+}>;
+
+type InferRequest<T extends RequestSchema> = {
+  params: z.infer<T>["params"] extends undefined ? object : z.infer<T>["params"];
+  body: z.infer<T>["body"] extends undefined ? unknown : z.infer<T>["body"];
+  query: z.infer<T>["query"] extends undefined ? object : z.infer<T>["query"];
+};
+
 const validationMiddleware =
-  <T extends z.ZodType<{ body?: unknown; query?: unknown; params?: unknown }>>(
+  <T extends RequestSchema>(
     schema: T
   ): RequestHandler<
-    z.infer<T>["params"] extends undefined ? object : z.infer<T>["params"],
+    InferRequest<T>["params"],
     unknown,
-    z.infer<T>["body"] extends undefined ? unknown : z.infer<T>["body"],
-    z.infer<T>["query"] extends undefined ? object : z.infer<T>["query"]
+    InferRequest<T>["body"],
+    InferRequest<T>["query"]
   > =>
   async (req, _, next) => {
     try {
-      await schema.parseAsync({
-        body: req.body,
-        query: req.query,
-        params: req.params
-      });
+      schema.parse(req);
       return next();
     } catch (error) {
       if (error instanceof ZodError) {
