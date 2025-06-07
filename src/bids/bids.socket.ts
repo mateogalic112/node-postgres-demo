d5d1008b0c4be564bddf0de1e66e640a17c3f115
Mateo Galic <mateogalic112@gmail.com>
2025-06-07 13:34:30 +0200

    add try catch for sockets

--- src/bids/bids.socket.ts
+++ src/bids/bids.socket.ts
@@ -22,27 +22,29 @@ export class BidsSocketController extends SocketController {
   private async handleCreateBid(payload: CreateBidPayload) {
     if (!this.socket) return;
 
-    const cookieHeader = this.socket.handshake.headers.cookie;
-    if (!cookieHeader) {
-      this.socket.emit("bid:error", { message: "Authentication required" });
-      return;
+    try {
+      const cookieHeader = this.socket.handshake.headers.cookie;
+      if (!cookieHeader) {
+        this.socket.emit("bid:error", { message: "Authentication required" });
+        return;
+      }
+
+      const token = cookieHeader.split("Authentication=")[1];
+      if (!token) {
+        this.socket.emit("bid:error", { message: "Authentication required" });
+        return;
+      }
+
+      const user = await this.authService.extractUserFromToken(token);
+      if (!user) {
+        this.socket.emit("bid:error", { message: "Authentication required" });
+        return;
+      }
+
+      const bid = await this.bidService.createBid(user, payload);
+      this.socket.emit("bid:created", bid);
+    } catch (error) {
+      this.socket.emit("bid:error", { message: (error as Error).message });
     }
-
-    const token = cookieHeader.split("Authentication=")[1];
-    if (!token) {
-      this.socket.emit("bid:error", { message: "Authentication required" });
-      return;
-    }
-
-    console.log({ token });
-
-    const user = await this.authService.extractUserFromToken(token);
-    if (!user) {
-      this.socket.emit("bid:error", { message: "Authentication required" });
-      return;
-    }
-
-    const bid = await this.bidService.createBid(user, payload);
-    this.socket.emit("bid:created", bid);
   }
 }
