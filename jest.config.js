d1a361984567c022b5482f620fe1ee9941f8c2cc
Mateo Galic <mateogalic112@gmail.com>
2026-03-01 14:27:38 +0100

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,14 @@ export default {
   globalSetup: "<rootDir>/src/__tests__/globalSetup.ts",
   globalTeardown: "<rootDir>/src/__tests__/globalTeardown.ts",
   setupFilesAfterEnv: ["<rootDir>/src/__tests__/jestSetup.ts"],
-  modulePaths: ["<rootDir>/src"]
+  modulePaths: ["<rootDir>/src"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/__tests__/**",
+    "!src/database/migrations.ts",
+    "!src/database/rollback.ts",
+    "!src/database/seed.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "clover"],
 };
