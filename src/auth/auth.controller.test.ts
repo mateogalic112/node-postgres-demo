22b3e11fd2d5096d53a43e475e51bd355bfedf57
Mateo Galic <mateogalic112@gmail.com>
2026-03-04 20:00:47 +0100

    update auth tests

--- src/auth/auth.controller.test.ts
+++ src/auth/auth.controller.test.ts
@@ -6,7 +6,6 @@ import { UsersRepository } from "users/users.repository";
 import { UserService } from "users/users.service";
 import { getTestClient, registerUserRequest } from "__tests__/setup";
 import { createMockDatabaseService } from "__tests__/mocks";
-import { createMockedLoginPayload, createMockedRegisterPayload } from "./mocks/auth.mocks";
 import { RolesRepository } from "roles/roles.repository";
 import { StripeService } from "services/stripe.service";
 import { ProductRepository } from "products/products.repository";
@@ -26,31 +25,20 @@ describe("AuthController", () => {
 
   describe("POST /api/v1/auth/register", () => {
     it("should register a new user", async () => {
-      const mockedRegisterPayload = createMockedRegisterPayload("testuser", "password");
-
-      const response = await request(app.getServer())
-        .post("/api/v1/auth/register")
-        .send(mockedRegisterPayload);
+      const response = await registerUserRequest(app, "testuser");
 
       expect(response.status).toBe(201);
       expect(response.headers["set-cookie"]).toBeDefined();
-      expect(response.body.data.username).toBe(mockedRegisterPayload.username);
+      expect(response.body.data.username).toBe("testuser");
     });
 
     it("should return 400 if user already exists", async () => {
-      const mockedRegisterPayload = createMockedRegisterPayload("testuser", "password");
-
-      const response = await request(app.getServer())
-        .post("/api/v1/auth/register")
-        .send(mockedRegisterPayload);
-
-      const response2 = await request(app.getServer())
-        .post("/api/v1/auth/register")
-        .send(mockedRegisterPayload);
+      const response = await registerUserRequest(app, "testuser");
+      const response2 = await registerUserRequest(app, "testuser");
 
       expect(response.status).toBe(201);
       expect(response.headers["set-cookie"]).toBeDefined();
-      expect(response.body.data.username).toBe(mockedRegisterPayload.username);
+      expect(response.body.data.username).toBe("testuser");
 
       expect(response2.status).toBe(400);
       expect(response2.body).toHaveProperty("message", "User with that email already exists");
@@ -59,40 +47,37 @@ describe("AuthController", () => {
 
   describe("POST /api/v1/auth/login", () => {
     it("should NOT login a user if user does not exist", async () => {
-      const mockedLoginPayload = createMockedLoginPayload("testuser", "password");
-
-      const response = await request(app.getServer())
-        .post("/api/v1/auth/login")
-        .send(mockedLoginPayload);
+      const response = await request(app.getServer()).post("/api/v1/auth/login").send({
+        email: "testuser@example.com",
+        password: "password"
+      });
 
       expect(response.status).toBe(400);
       expect(response.body.message).toBe("Invalid email or password");
     });
 
     it("should NOT login a user if password is incorrect", async () => {
-      const mockedRegisterPayload = createMockedRegisterPayload("testuser", "password");
-      await registerUserRequest(app, mockedRegisterPayload.username);
+      await registerUserRequest(app, "testuser");
 
-      const mockedLoginPayload = createMockedLoginPayload("testuser", "invalid-password");
-      const response = await request(app.getServer())
-        .post("/api/v1/auth/login")
-        .send(mockedLoginPayload);
+      const response = await request(app.getServer()).post("/api/v1/auth/login").send({
+        email: "testuser@example.com",
+        password: "invalid-password"
+      });
 
       expect(response.status).toBe(400);
       expect(response.body.message).toBe("Invalid email or password");
     });
 
     it("should login an existing user with correct credentials", async () => {
-      const username = "testuser";
-      await registerUserRequest(app, username);
+      await registerUserRequest(app, "testuser");
 
-      const mockedLoginPayload = createMockedLoginPayload(username, "password");
-      const response = await request(app.getServer())
-        .post("/api/v1/auth/login")
-        .send(mockedLoginPayload);
+      const response = await request(app.getServer()).post("/api/v1/auth/login").send({
+        email: "testuser@example.com",
+        password: "password"
+      });
 
       expect(response.status).toBe(200);
-      expect(response.body.data.username).toBe(username);
+      expect(response.body.data.username).toBe("testuser");
     });
   });
 });
