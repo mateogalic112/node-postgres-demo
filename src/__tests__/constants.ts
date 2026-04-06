af36b82b25a59bf7b648ee4f83f6f03d67f1426b
Mateo Galic <mateogalic112@gmail.com>
2026-04-06 23:15:46 +0200

    remove test roles

--- src/__tests__/constants.ts
+++ src/__tests__/constants.ts
@@ -1,12 +1,5 @@
-import { RoleName } from "../roles/roles.validation";
-
 export const TEST_ADMIN_USER = {
   username: "admin",
   email: "admin@example.com",
   password: "password"
 };
-
-export const TEST_ROLES = new Map<RoleName, string>([
-  [RoleName.ADMIN, "Admin role"],
-  [RoleName.USER, "User role"]
-]);
